3b82d9b0cadf651d56af34bd69c32421eacb846a
Toth Levente <leventetoth9@gmail.com>
2022-08-02 16:09:54 +0300

    jh

--- pages/index.tsx
+++ pages/index.tsx
@@ -103,6 +103,7 @@ const Home: NextPage = () => {
         <h2>Hello3</h2>
         <h2>Hello4</h2>
         <h2>Hello5</h2>
+        <h2>Hello6</h2>
 
         <p className={styles.description}>
           Get started by editing{' '}
